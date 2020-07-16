ab3bf29e77d7812636c668cb2458cfa76c1b6348
Ahmed Sheikh <66457087+ASheikh-io@users.noreply.github.com>
2020-07-15 23:56:49 -0400

    updated script

--- script.js
+++ script.js
@@ -1,100 +1,66 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//provided var, this traverses the DOM to grab the generate ID, changed to const
 const generateBtn = document.querySelector("#generate");
+// hook onto the id's of the checked boxes to later evaluate if it has been checked, chose getElementById instead of querySelector as I was running into issues with querySelector and getting .checked to work
+let uppercaseCheck = document.getElementById("uppercase");
+let lowercaseCheck = document.getElementById("lowercase");
+let numberCheck = document.getElementById("number");
+let symbolCheck = document.getElementById("symbol");
+// gets
+const passwordLength = document.querySelector("#passwordLength");
+const printPassword = document.querySelector("#password");
+// strings that hold password options that are turned into arrays by .split
+const lowercaseStr = "abcdefghijklmnopqrstuvwxyz".split("");
+const uppercaseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const symbolStr = "~!@#$%^&*()_+`-={}|[]:;'<>?,./".split("");
+const numberStr = "1234567890".split("");
 
- // Add event listener to button and calling function
- generateBtn.addEventListener("click", function() {
-  passwordDecisions();
-  });
+// creates a source array based on what checkboxes are checked by user that will be used in the generate password function
+function createSourceArr() {
+  // empty array that will have strings added onto it depending on what boxes have been checked
+  let sourceArr = [];
 
-// Function for prompts and adding password to textvalue
-function passwordDecisions() {
-
-// Assignment Code for strings and arrays
-  let passwd = [];
-  let passArray = [];
-  const lower = "abcdefghijklmnopqrstuvwxyz";
-  const lowerArr = lower.split("");
-  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const upperArr = upper.split("");
-  const num = "0123456789";
-  const numArr = num.split("");
-  const speChar = "!#$%&'()*+,'()*+,-./:;<=>?@[\]^_`{|}~"
-  const speCharArr = speChar.split("");
-
-
-// Resets final password
-  let finalPassword = "";
-
-  // Write password to the #password input
-  let passwordText = document.querySelector("#password");
-  passwordText.value = finalPassword;
-
-  // password length 
-  let passwordLength = prompt("What is the length of your password?");
-  if ((passwordLength < 8) || (passwordLength > 128)) {
-    alert("Please enter a valid length for the password with at least 8 characters and no more then 128 characters.");
-    return;
+  
+  // set of if statements that will push their arrays onto the sourceArr if it is true that the corresponding box has been checked in the HTML
+  if (lowercaseCheck.checked) {
+    // spread operator is allowing sourceArr to be expanded by .push
+    sourceArr.push(...lowercaseStr);
   }
- 
-  // password length must be a valid numberical length
-  // isNan() returns true when input is not a number (checks user input is a number)
-  if (isNaN(passwordLength)) {
-    alert("Please enter a valid numerical length for the password with at least 8 characters and no more than 128 characters.");
-    return;
+  if (uppercaseCheck.checked) {
+    sourceArr.push(...uppercaseStr);
   }
-  
-  // Declaring variables for other 4 password options
-  const lowerCase = confirm("Do you want lower case letters in your password?");
-  const upperCase = confirm("Do you want upper case letters in your password?");
-  const numCase = confirm("Do you want numbers in your password?");
-  const specCase = confirm("Do you want special characters in your password?");
-  
-  // Adding lower case, upper case, numbers, and special characters to arrays
-  if (lowerCase === true) {
-    passArray = passArray.concat(lowerArr)
-   
+  if (symbolCheck.checked) {
+    sourceArr.push(...symbolStr);
   }
-  if (upperCase === true) {
-    passArray = passArray.concat(upperArr);
-    
-    } 
-  if (numCase === true) {
-    passArray = passArray.concat(numArr);
-      
-    }
-  if (specCase === true) {
-    passArray = passArray.concat(speCharArr);
-     
-    }
-  // Alerting user that they must choose at least one of the options for a password
-  if ((lowerCase === false) && (upperCase === false) && (numCase === false) && (specCase === false)) {
-      alert("Please chose at least one of the options for your password.");
-    }
-   
-  
-    // for loop to create a random password with the correct length and options
-  const passwordArrayLength = passArray.length;
-  for (let i = 0; i < passwordLength; i++) {
-    
-    let specialNum = Math.ceil(Math.random()*passwordArrayLength);
-    passwd.push(passArray[specialNum]);
+  if (numberCheck.checked) {
+    sourceArr.push(...numberStr);
   }
-  finalPassword = passwd.join('');
-  
-  
-  // Writes new password to the #password input
-  passwordText.value = finalPassword;
+  // the function will return the completed source array with all data
+  return sourceArr;
 }
+
+// this function generates the password based on the source array and listens for length of the password
+function generatePassword() {
+  // allows the use of the array created based on user preferences
+  let createdSourceArray = createSourceArr();
+  // gets the value based on user input
+  desiredPasswordLength = passwordLength.value;
+
+  // checks to make sure the password is between 8 and 128, otherwise alerts an error
+  if (desiredPasswordLength < 8 || desiredPasswordLength > 128) {
+    alert("Please enter a value between 8 - 128.");
+  } else {
+    // empty array for randomized characters based on user length
+    let generatedPassword = [];
+    // randomizes however many characters user wants
+    for (let i = 0; i < desiredPasswordLength; i++) {
+      const index = Math.floor(Math.random() * createdSourceArray.length);
+      // final password array is then concatinated to one string
+      generatedPassword = generatedPassword.concat(createdSourceArray[index]);
+    }
+    // changes innerHTML of textarea element to show generated password
+    printPassword.innerHTML = generatedPassword.join("");
+  }
+}
+
+// when generate button is clicked, run the generate password function and print it to the screen
+generateBtn.addEventListener("click", generatePassword);
